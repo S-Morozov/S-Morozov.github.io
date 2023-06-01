302f685c4dfe1885fb9d3ce10918f74521457821
Semen Morozov <semenmorozovsuomi@gmail.com>
2023-06-01 14:43:29 +0300

    php modified

--- mail.php
+++ mail.php
@@ -1,18 +1,34 @@
 <?php
+// Check if the form is submitted
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Get the form fields
   $name = $_POST['name'];
   $email = $_POST['email'];
-  $message = $_POST['message'];
   $subject = $_POST['subject'];
+  $message = $_POST['message'];
 
-    $to = 'semenmorozov24@gmail.com';
+  // Set the recipient email address
+  $to = 'semenmorozov24@gmail.com';
 
-    $txt = "Name = ". $name . "\r\n Email = " . $email . "\r\n Message = " . $message . "\r\n subject =" . $subject;
+  // Set the email headers
+  $headers = "From: $name <$email>\r\n";
+  $headers .= "Reply-To: $email\r\n";
+  $headers .= "Content-Type: text/plain; charset=UTF-8\r\n";
 
-    $headers = "From: noreply@codeconia.com" . "\r\n" .
-    "CC: somebodyelse@example.com";
-    if($email !=NULL) {
-        mail($to, $subject, $txt, $headers);
-    }
+  // Compose the email body
+  $email_body = "Name: $name\n";
+  $email_body .= "Email: $email\n";
+  $email_body .= "Subject: $subject\n";
+  $email_body .= "Message: $message\n";
 
-    header('Location: thankyoumail.html');
+  // Send the email
+  if (mail($to, $subject, $email_body, $headers)) {
+    // Email sent successfully
+    header('Location: thankyoumail.html'); // Redirect to the thank you page
+    exit();
+  } else {
+    // Failed to send email
+    echo 'An error occurred while sending the email.';
+  }
+}
 ?>
